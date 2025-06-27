0a9e4164f8a24a65980ef13114e7bb01d85f61c9
unknown <KM59135@dir.kmt.kmtg.net>
2025-06-27 17:43:34 +0900

    done

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -18,7 +18,7 @@ const getPremiumContent = async (id: number, supabase: SupabaseClient<Database>)
 };
 
 
-const SolutionDetailPage = async ({params}: {params: {id: number}}) => {
+const SolutionDetailPage = async ({params}: {params: {id: number;};}) => {
     const supabase = await createServerComponentClient<Database>({ cookies });
     const [solution, content] = await Promise.all([
         await getDetailSolution(params.id, supabase),
@@ -39,6 +39,3 @@ const SolutionDetailPage = async ({params}: {params: {id: number}}) => {
 };
 
 export default SolutionDetailPage;
-
-//PowerBIレポートの埋め込みタグ
-//<iframe title="KEISA_Riotinto_HD785_Kplus_Report_BO_r3" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=d9934bc2-73fb-4769-b232-62779eb82387&autoAuth=true&ctid=58be8688-6625-4e52-80d8-c17f3a9ae08a&actionBarEnabled=true" frameborder="0" allowFullScreen="true"></iframe>
